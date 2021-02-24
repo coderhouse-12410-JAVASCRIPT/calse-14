57fcba24a789fba974749716e6e14f71137003f0
alejandro medici <el.aleyo@gmail.com>
2021-02-23 21:55:27 -0300

    now with $.ajax version

--- js/app.js
+++ js/app.js
@@ -1,7 +1,19 @@
-$("button").click(function(){
+$("#get").click(function(){
     $.get("https://coderhouse-12410-javascript.github.io/data/data.json", 
         function(data, status){
             console.log("mi data es: " + data + " mi status code es: " + status);
         }
     )
+});
+
+$("#ajax").click(function(){
+    $.ajax({
+        url: "https://coderhouse-12410-javascript.github.io/data/data.json",
+        dataType: "json",
+        type: "GET", // Por defecto Ajax usa Get
+        success: function(data){
+            console.log("mi data es: " + data );
+        }
+    }
+    );
 });
