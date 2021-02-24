411a55f205125f0767209e6bd4bddac39489e827
alejandro medici <el.aleyo@gmail.com>
2021-02-23 22:00:34 -0300

    last changes

--- js/app.js
+++ js/app.js
@@ -1,18 +1,18 @@
-$("#get").click(function(){
-    $.get("https://coderhouse-12410-javascript.github.io/data/data.json", 
-        function(data, status){
+$("#get").click(function () {
+    $.get("https://coderhouse-12410-javascript.github.io/data/data.json",
+        function (data, status) {
             console.log("mi data es: " + data + " mi status code es: " + status);
         }
     )
 });
 
-$("#ajax").click(function(){
+$("#ajax").click(function () {
     $.ajax({
         url: "https://coderhouse-12410-javascript.github.io/data/data.json",
         dataType: "json",
         type: "GET", // Por defecto Ajax usa Get
-        success: function(data){
-            console.log("mi data es: " + data );
+        success: function (data) {
+            console.log("mi data es: " + data);
         }
     }
     );
